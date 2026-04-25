188296cd28a27b551f0d21f88aea15f407874a5b
magnusculley <magnus@udel.edu>
2026-04-25 03:15:32 -0400

    fixed

--- run.sh
+++ run.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
-#SBATCH -A grp_202
-#SBATCH -p ghx4
+#SBATCH -A bffp-delta-gpu
+#SBATCH -p gpuA40x4
 #SBATCH --ntasks-per-node=1
 #SBATCH --nodes=1
 #SBATCH --cpus-per-task=5
 #SBATCH --gres=gpu:4
 #SBATCH -t 00:10:00
 
-./jacobi 8192 8192
-
-./jacobi 16384 16384
-
+./a.out 8192 8192
+./a.out 16384 16384
 ./pi 1000000
